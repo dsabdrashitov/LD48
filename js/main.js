ba35ef9055b3b67d6c3da593d01e6c9eece33033
DarLam <domanocybi@gmail.com>
2021-04-24 15:47:03 +0300

    Painted rocket on screen.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+const Sprite = PIXI.Sprite;
+
 let app = new PIXI.Application();
 document.body.appendChild(app.view);
+
+PIXI.loader
+  .add("images/main-sheet.json")
+  .load(resourcesLoaded);
+
+let textures;
+
+function resourcesLoaded() {
+  textures = PIXI.loader.resources["images/main-sheet.json"].textures;
+  let rocket = new Sprite(textures["rocket.png"]);
+  rocket.x = app.stage.width / 2;
+  rocket.y = app.stage.height / 2;
+  app.stage.addChild(rocket);
+}
